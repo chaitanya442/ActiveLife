afb94bdb193c6143666d472e575d69d8c1eda262
Chaitanya Chandu <chaitanyac140@gmail.com>
2025-09-18 19:23:12 +0000

    try to implement this in graphs for easy understanding

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -175,12 +175,26 @@ export default function DashboardPage() {
                   cursor={false}
                   content={<ChartTooltipContent indicator="dot" />}
                 />
+                <defs>
+                  <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                    <stop
+                      offset="5%"
+                      stopColor="var(--color-desktop)"
+                      stopOpacity={0.8}
+                    />
+                    <stop
+                      offset="95%"
+                      stopColor="var(--color-desktop)"
+                      stopOpacity={0.1}
+                    />
+                  </linearGradient>
+                </defs>
                 <Area
                   dataKey="desktop"
                   type="natural"
-                  fill="var(--color-desktop)"
-                  fillOpacity={0.4}
+                  fill="url(#fillDesktop)"
                   stroke="var(--color-desktop)"
+                  stackId="a"
                 />
               </AreaChart>
             </ChartContainer>
